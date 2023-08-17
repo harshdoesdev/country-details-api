bfe81f090d277e0cbe361ca4d777f1d17fa63f2e
Harsh Singh <harshsingh.js@gmail.com>
2023-08-17 11:01:58 +0530

    Fixed capital names

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ import { readFileSync } from 'fs';
 
 import { cwd } from 'process';
 
+import cors from 'cors';
+
 const dir = cwd();
 
 const PORT = 8080;
 
 const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(express.json());
 
@@ -17,6 +23,14 @@ const data = JSON.parse(readFileSync(`${dir}/data.json`).toString());
 app.get('/', (req, res) => {
 
     return res.status(200).json(data);
+
+});
+
+app.post('/', (req, res) => {
+
+    console.log('Stuff works', req.body);
+
+    return res.status(200).json({});
 
 });
 
